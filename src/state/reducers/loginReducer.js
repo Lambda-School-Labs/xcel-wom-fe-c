4515de07e9ac47cd83d0aa4674bf5acfa4942bba
Ryan Schueck <ryschueck@gmail.com>
2021-01-05 20:34:31 -0500

    created initial state for the loginReducer

---
+++ src/state/reducers/loginReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  isLoading: false,
+  userName: '',
+  password: '',
+  error: false,
+  errorMessage: 'Incorrect Username or Password',
+  success: false,
+  token: '',
+};
+
+const loginReducer = (state = initialState, action) => {};
+
+export default loginReducer;
